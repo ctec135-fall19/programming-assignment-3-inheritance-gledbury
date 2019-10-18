f30cfb03d357604e4e065b96b179a3c42fe9940e
Gledbury <gledbury4@gmail.com>
2019-10-18 15:03:28 -0700

    Ledbury Problem1

---
+++ Problem1/Character.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem1
+{
+    public abstract class Character
+    {
+        public void display()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Problem1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Author: George Ledbury
+Date: 10/18/19
+CTEC 135: Microsoft Software Development with C#
+Programming Assignment 3, Problem 1
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
