dc5d7ebae3c014529d4e6a4bbf92a75ab1f3c564
Gledbury <gledbury4@gmail.com>
2019-10-25 09:23:42 -0700

    assignment 3 finished

--- Prob3/Program.cs
+++ Prob3/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+Author: George Ledbury
+Date:   10/23/2019
+CTEC 135: Microsoft Software Development with C#
+​
+Module 4: Programming Assignment 3, Problem 3
+  * Demonstrate both the aggregation and composition relationships
+  * 
+*/
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
